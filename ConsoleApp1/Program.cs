0484104774df949443fd5808cb0b8ff408c5fe86
Shnur <Shnur@DESKTOP-05I0049>
2022-05-15 13:46:57 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Консоль.НапишиСтроку
+            Console.WriteLine("Hello World!!!");
+
+            //Delay
+            Console.ReadLine();
+          
+
+
+
+        }
+
+    }
+}
